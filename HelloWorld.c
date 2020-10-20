91604d76df1527e0359f08c0853765488025566b
Segmentational <jacob.sanders@cloudhybrid.io>
2020-10-19 20:37:33 -0500

    Refactoring + Print.c

--- HelloWorld.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int helloWorld(){
-	printf("Hello World");
-}
---
+++ Read.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+char* stdin(){
+	char* input; 
+	printf("Enter a String: ");
+	scanf("%s %d", &input);
+	return input
+}
