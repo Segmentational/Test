5a89d1666e6bef463892e6fdb2577a46b4b574e8
Segmentational <jacob.sanders@cloudhybrid.io>
2020-10-19 21:06:34 -0500

    Refactor

--- Read.c
+++ Read.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
-char* stdin(){
-	char* input; 
+#include <stdio.h>
+#include <stdlib.h>
+
+char* read() {
+	// int size = sizeof(input / input[0]);
+
+	char input[100];
+
 	printf("Enter a String: ");
-	scanf("%s %d", &input);
-	return input
+	scanf("%s", input);
+	printf("%s: %p", "Memory Address from Input", &input);
+
+	return &input;
 }
